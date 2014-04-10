650bf00df4ebf656a7d42ec26adcd2abd0730ef9
Luan Cestari <luan.cestari@gmail.com>
2014-04-10 16:36:54 -0300

    Fixes

--- puppet_installation/modules/facts/lib/facter/required_facts.rb
+++ puppet_installation/modules/facts/lib/facter/required_facts.rb
@@ -1,14 +1,14 @@
-Facter.add(:REMOTE_USER) do
+Facter.add(:remoteuser) do
   setcode do
     "FREMOTE_USER"
   end
 end
-Facter.add(:RHN_USER) do
+Facter.add(:rhnuser) do
   setcode do
     "FRHN_USER"
   end
 end
-Facter.add(:RHN_PASS) do
+Facter.add(:rhnpass) do
   setcode do
     "FRHN_PASS"
   end
--- puppet_installation/modules/lightblue/manifests/nodes.pp
+++ puppet_installation/modules/lightblue/manifests/nodes.pp
@@ -3,12 +3,12 @@ node default {
     RedHat  : { $supported = true }
     default : { fail("The ${module_name} module is not supported on ${::osfamily} based systems") }
   }
-  notice("The \$username value is: ${::RHN_PASS} // $::RHN_PASS ")
+
   include croot
   include puppet
   class { 'rhn':
-    username => $::RHN_USER,
-    password => $::RHN_PASS,
+    username => $::rhnuser,
+    password => $::rhnpass,
     force    => true,
   }
 }
