59123595711c302ea5664d699b5dd49b9924145b
Luan Cestari <luan.cestari@gmail.com>
2014-04-11 14:38:08 -0300

    Fixes

--- puppet_installation/modules/lightblue/manifests/nodes.pp
+++ puppet_installation/modules/lightblue/manifests/nodes.pp
@@ -45,20 +45,41 @@ node default {
     password_hash => 'a15fbfca5e3a758be80ceaf42458bcd8',
   }
 
+  file { "/usr/src/lightblue":
+    ensure => directory,
+    purge => true,
+    force => true,
+    before => 'git_lightblue',
+  }
+
+  git::repo{'git_lightblue':
+    path   => "/usr/src/lightblue",
+    source => 'git://github.com/lightblue-platform/lightblue.git'
+  }
+
   exec { 'build-lightblue':
     path    => $::path,
     user    => $::remoteuser,
+    cwd     => "/usr/src/lightblue",
+    command => 'mvn clean install',
+    path    => "/usr/local/bin/:/usr/bin:/bin/",
+  }
+
+  exec { 'build-lightblue':
+    path    => $::path,
     cwd     => "/home/${::remoteuser}/",
-    command => 'rm -rf lightblue && git clone https://github.com/lightblue-platform/lightblue && cd lightblue && mvn -q clean install && mv rest/crud/target/rest-crud* /tmp/rest-crud.war && mv rest/metadata/target/rest-metadata* /tmp/rest-metadata.war ',
+    command => 'mv /home/${::remoteuser}/lightblue/rest/crud/target/rest-crud* /tmp/rest-crud.war && mv /home/${::remoteuser}/lightblue/rest/metadata/target/rest-metadata* /tmp/rest-metadata.war ',
   }
 
   jboss_as::deploy { 'rest-metadata.war':
     pkg         => 'rest-metadata.war', 
-    tmp     => true,
+    tmp         => true,
+    jboss_use   => 'jboss',
   }
   jboss_as::deploy { 'rest-crud.war':
     pkg         => 'rest-crud.war', 
-    tmp     => true,
+    tmp         => true,
+    jboss_user  => 'jboss',
   }
 
 }
