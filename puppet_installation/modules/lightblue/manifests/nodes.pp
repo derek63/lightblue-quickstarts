b1bf75ed4300907e07e4d9191873708ec4384c78
Luan Cestari <luan.cestari@gmail.com>
2014-04-14 09:17:15 -0300

    Fixes

--- puppet_installation/modules/lightblue/manifests/nodes.pp
+++ puppet_installation/modules/lightblue/manifests/nodes.pp
@@ -37,10 +37,6 @@ node default {
     password => $::rhnpass,
   }->
 
-  class {'::mongodb::server':
-    auth => true,
-  }->
-
   mongodb::db { 'testdb':
     user          => 'user1',
     password_hash => 'a15fbfca5e3a758be80ceaf42458bcd8',
