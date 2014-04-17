93f59efa996683c7482e6136a3657375fab61ab7
Luan Cestari <luan.cestari@gmail.com>
2014-04-17 14:44:47 -0300

    Fixes v2

--- puppet_installation/modules/lightblue/manifests/nodes.pp
+++ puppet_installation/modules/lightblue/manifests/nodes.pp
@@ -16,7 +16,7 @@ node default {
   }->
 
   package { 'java-1.7.0-openjdk-devel':
-    ensure => installed,
+    ensure   => installed,
   }->
 
 
@@ -27,37 +27,78 @@ node default {
     jboss_home     => '/usr/share/jboss',
     staging_dir    => '/tmp/puppet-staging/jboss_as',
     standalone_tpl => 'jboss_as/standalone.xml.erb',
-    download => true,
-    username => $::rhnuser,
-    password => $::rhnpass,
+    download       => true,
+    username       => $::rhnuser,
+    password       => $::rhnpass,
   }->
+
   package { 'mongodb-server.x86_64':
     ensure => installed,
   }->
+
   package { 'mongodb.x86_64':
     ensure => installed,
   }->
+
   exec { 'install the rest-metadata':
     command => "rpm -Fiv /tmp/rest-metadata.rpm || true",
     path    => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
     require =>  Class['jboss_as'],
   }->
+
   exec { 'install the rest-crud':
     command => "rpm -Fiv /tmp/rest-crud.rpm || true",
-    path => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
+    path    => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
     require =>  Class['jboss_as'],
   }->
+
+  jboss_as::jbossmodule{'Set jboss module directory':
+   jboss_home => '/usr/share/jboss',
+   moduledir  => 'com.redhat.lightblue',
+  }->
+
+  #You may use variables (like $template_module_path in jbossmodule) to set dynamic content
+  class{'jboss_as::jbossmodulefile':
+   jboss_home         => '/usr/share/jboss',
+   moduledir          => 'com.redhat.lightblue',
+   configuration_file => 'config.properties',
+   owner              => 'jboss_as',
+   group              => 'jboss_as',
+   content            => template('config.properties.erb'),
+  }->
+
+  class{'jboss_as::jbossmodulefile':
+   jboss_home         => '/usr/share/jboss',
+   moduledir          => 'com.redhat.lightblue',
+   configuration_file => 'lightblue-crud.json',
+   owner              => 'jboss_as',
+   group              => 'jboss_as',
+   content            => template('lightblue-crud.json.erb'),
+  }->
+
+  class{'jboss_as::jbossmodulefile':
+   jboss_home         => '/usr/share/jboss',
+   moduledir          => 'com.redhat.lightblue',
+   configuration_file => 'lightblue-metadata.json',
+   owner              => 'jboss_as',
+   group              => 'jboss_as',
+   content            => template('lightblue-metadata.json.erb'),
+  }->
+
   exec { 'config mongodb.conf: smallfiles':
     command => "echo 'smallfiles = true' | tee -a /etc/mongodb.conf",
-    path => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
-    unless => "grep --quiet smallfiles /etc/mongodb.conf 2>/dev/null"
+    path    => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
+    unless  => "grep --quiet smallfiles /etc/mongodb.conf 2>/dev/null"
   }->
+
   service { "mongod":
     ensure => "running",
   }->
+
   exec { 'Network':
     command => "iptables -I INPUT 1 -p tcp --dport 8080 -j ACCEPT",
-    path => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
-    unless => "iptables -L | grep --quiet 'tcp dpt:webcache'  2>/dev/null"
+    path    => ['/usr/bin', '/bin', '/sbin', '/usr/sbin'],
+    unless  => "iptables -L | grep --quiet 'tcp dpt:webcache'  2>/dev/null"
   }
+
 }
