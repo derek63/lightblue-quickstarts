7d05c947d68e6fadedcc33c34ebafbbaa7bcb365
Luan Cestari <luan.cestari@gmail.com>
2014-05-14 09:08:40 -0300

    Updated the wizard_puppet.sh to work on RHEL (and probably forks) OS and made the timeout to download EAP bigger (as I got some slow connections)

--- wizard_puppet.sh
+++ wizard_puppet.sh
@@ -2,9 +2,21 @@
 
 # This file intead to make easier the use of the enviroment variables (especially for the first time or single use. The user might also set the enviroment variable by his one on the current shell so the rake will be able to work fine
 
+# THe following line is to install lastest packages in teh user enviroment in Non-Fedora OS (using rvm)
+OSID=$(lsb_release -si)
+if ["$OSID" -ne "Fedora"]; then
+	curl -sSL https://get.rvm.io | bash -s stable
+	source /home/ec2-user/.rvm/scripts/rvm
+	rvm install 1.9.3
+	ruby -v
+	gem -v
+	sudo yum install -q -y git  java-1.7.0-openjdk-devel.x86_64 @development-tools rpm-build #needed to build the project (git to clone. java, maven and the rpm-build to build and run 
+fi
+
+
 echo "Checking missing files to run the script"
-sudo yum -y install gcc ruby ruby-devel rubygems rake git # You can remove this line later. ALso, you can choose to not install puppet if you want the git hook validation
-sudo gem install aws-sdk # it is necessary to create aws instances
+sudo yum -y install gcc ruby ruby-devel rubygems rake git # You can remove this line later. ALso, you can choose to not install puppet if you want the git hook validation. #Also, this is needed to run on Fedora, as it has the most recent packages 
+gem install aws-sdk # it is necessary to create aws instances
 
 echo "Please enter the path to the PEM file to access the aws's ec2 instance (hit enter for default, ~/.ssh/soa.pem):"
 read    PATH_PEM_KEY
